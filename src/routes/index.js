4887e7402297a3a4539e1c077913df524e3e0dab
Rodney Lab <codesign@rodneylab.com>
2022-07-08 09:49:16 +0100

    fix: 💫 remove node:fs from blog utilities for wider compatibility

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,8 +1,14 @@
-import { getPosts, getPostsContent } from '$lib/utilities/blog';
-
 /** @type {import('./index').RequestHandler} */
 export async function get() {
-  const postsContent = getPostsContent();
-  const posts = await getPosts(postsContent, false);
+  const mdModules = import.meta.glob('../content/blog/**/index.md');
+  const posts = await Promise.all(
+    Object.keys(mdModules).map(async (path) => {
+      const slug = path.split('/').at(-2);
+      const { metadata } = await mdModules[path]();
+      const { datePublished, lastUpdated, postTitle, seoMetaDescription } = metadata;
+      return { datePublished, lastUpdated, postTitle, seoMetaDescription, slug };
+    }),
+  );
+
   return { body: { ...{ posts } } };
 }
